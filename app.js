0f8fbc7fbe253f76839a6ed3826772a0968accfa
maqalher <maqalher@hotmail.com>
2020-02-24 15:38:16 -0600

    add delete & update

--- app.js
+++ app.js
@@ -44,7 +44,61 @@ db.collection("users").onSnapshot((querySnapshot) => {
 		      <td>${doc.data().first}</td>
 		      <td>${doc.data().last}</td>
 		      <td>${doc.data().born}</td>
+		      <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
+		      <td><button class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().first}','${doc.data().last}','${doc.data().born}')">Editar</button></td>
 		    </tr>
         `;
     });
 });
+
+
+//Borrar documentos
+function eliminar(id){
+	db.collection("users").doc(id).delete().then(function() {
+	    console.log("Document successfully deleted!");
+	}).catch(function(error) {
+	    console.error("Error removing document: ", error);
+	});
+}
+
+//Editar documento
+function editar(id, nombre, apellido, fecha) {
+
+	document.getElementById('nombre').value = nombre;
+	document.getElementById('apellido').value = apellido;
+	document.getElementById('fecha').value = fecha;
+	var boton = document.getElementById('boton');
+	boton.innerHTML = 'Editar';
+
+	boton.onclick = function() {
+		var washingtonRef = db.collection("users").doc(id);
+
+		// Set the "capital" field of the city 'DC'
+		
+		var nombre = document.getElementById('nombre').value;
+		var apellido = document.getElementById('apellido').value;
+		var fecha = document.getElementById('fecha').value;
+
+		return washingtonRef.update({
+		    first: nombre,
+			last: apellido,
+			born: fecha
+		})
+		.then(function() {
+		    console.log("Document successfully updated!");
+		    boton.innerHTML = 'Guardar';
+		    document.getElementById('nombre').value = '';
+			document.getElementById('apellido').value = '';
+			document.getElementById('fecha').value = ''; 
+		})
+		.catch(function(error) {
+		    // The document probably doesn't exist.
+		    console.error("Error updating document: ", error);
+		});	
+	}
+
+	
+}
+
+
+
